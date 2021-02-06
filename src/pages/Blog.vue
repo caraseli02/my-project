901620bd09bf693e6eb6904333040cf865986ad6
vlad <caraseli02@gmail.com>
2021-02-06 18:55:33 +0100

    set default blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,16 +1,15 @@
 <template>
   <Layout>
-    <vue-glide v-model="activeSlide" :options="options" class="">
-      <vue-glide-slide class="child w-screen h-screen" v-for="entry in $page.allBlog.edges" :key="entry.node.id">
+      <section class="panel" v-for="entry in $page.allBlog.edges" :key="entry.node.id">
         <div class="article-card bg-white overflow-hidden flex-1 relative">
-          <g-link class="featured-image-link block relative overflow-hidden" :to="entry.node.path">
+          <g-link class="featured-image-link block relative " :to="entry.node.path">
             <ul class="absolute top-0 left-0 flex mt-16 p-2 z-10">
               <li class="mr-2">
                 <span class="inline-block bg-pink-500 px-4 py-2 text-white text-xs font-bold rounded">{{
                   entry.node.category.title }}</span>
               </li>
             </ul>
-            <figure class="h-screen overflow-y-scroll">
+            <figure class="">
               <g-image class="" :alt="entry.node.image_caption" :src="entry.node.image" />
             </figure>
           </g-link>
@@ -29,40 +28,20 @@
             </div>
           </div>
         </div>
-      </vue-glide-slide>
-    </vue-glide>
+      </section>
   </Layout>
 </template>
 
 <script>
   // or all tools are exported from the "all" file (excluding bonus plugins):
   import { gsap, ScrollTrigger } from "gsap/all";
-  import { Glide, GlideSlide } from "vue-glide-js";
-  //gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
   export default {
     props: {},
     metaInfo: {
       title: "Blog",
     },
-    data() {
-      return {
-        activeSlide: 0,
-        options: {
-          autoplay: 5000,
-          bound: true,
-          breakpoints: {
-            450: {
-              perView: 1,
-            },
-            1024: {
-              perView: 2,
-            },
-          },
-        },
-      };
-    },
     mounted() {
-
     },
   };
 </script>
@@ -92,11 +71,6 @@
 </page-query>
 
 <style scoped lang="scss">
-  .container {
-    scroll-snap-type: mandatory;
-    scroll-snap-points-y: repeat(100vh);
-    scroll-snap-type: y mandatory;
-  }
 
   .child {
     scroll-snap-align: start;
